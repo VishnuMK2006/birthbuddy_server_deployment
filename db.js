11f72cea92eb306586c2c27378225f9d39163b92
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-06-27 19:16:00 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,27 +1,22 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://vishnu:tvmk2006@firstsample.c9yehfj.mongodb.net/?retryWrites=true&w=majority&appName=firstsample";
-
-//connect with db
-const connectToMongo = async () => {
-  try {
-    await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-};
-
-//schema define
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    dob: { type: String, required: true },
-    mobile: { type: Number, required: true }
+const userSchema = new mongoose.Schema({
+  name: String,
+  mobile: { type: String, unique: true },
+  dob: String, // Format: YYYY-MM-DD
+  groupType: { type: String, enum: ['public', 'private'], default: 'public' },
+  createdBy: { type: String, default: null }, // For private group creator
 });
 
-const User = mongoose.model('user', UserSchema);
+const mongoURI = "mongodb+srv://vishnu:tvmk2006@firstsample.c9yehfj.mongodb.net/?retryWrites=true&w=majority&appName=firstsample";
+const User = mongoose.model('User', userSchema);
 
-module.exports = {
-  connectToMongo,
-  User
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log("MongoDB Error: ", err));
 };
+
+module.exports = { connectToMongo, User };
