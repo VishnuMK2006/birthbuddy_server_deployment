027ed2807d4da62a03ecbe18cbc098d34728a62a
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-07-02 21:45:56 +0530

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -4,7 +4,7 @@ const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, enum: ['public'], default: 'public' },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  inviteCode: { type: String, required: true },
+  // inviteCode: { type: String, required: true },
   members: [
     {
       userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
